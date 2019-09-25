7bf26adf3a625725f0ffe9ba5e2487834a2ee939
agia3d <msn@agia3d.fr>
2019-09-26 00:59:59 +0200

    Update FastContextPlugin.java

--- Android/src/com/piqnt/fastcontext/FastContextPlugin.java
+++ Android/src/com/piqnt/fastcontext/FastContextPlugin.java
@@ -73,7 +73,7 @@ public class FastContextPlugin extends CordovaPlugin {
                 webView.getView().setBackgroundColor(Color.TRANSPARENT);
                 webView.getView().setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
             //    webView.setWebViewClient(new CordovaWebViewClient(cordova,
-                webView.setWebViewClient(new WebViewClient() {
+                ((WebView) webView.getView()).setWebViewClient(new WebViewClient() {
                     @Override
                     public void onPageFinished(WebView view, String url) {
                         webView.getView().setBackgroundColor(Color.TRANSPARENT);
