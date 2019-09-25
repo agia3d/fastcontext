d5ccd6e14a1bf268f536867c3caac5b872f5aeef
agia3d <msn@agia3d.fr>
2019-09-25 23:15:06 +0200

    CordovaWebViewCLient
    replaced by org.apache.cordova.engine.SystemWebViewClient

--- Android/src/com/piqnt/fastcontext/FastContextPlugin.java
+++ Android/src/com/piqnt/fastcontext/FastContextPlugin.java
@@ -4,7 +4,8 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.CordovaWebViewClient;
+//import org.apache.cordova.CordovaWebViewClient;
+import org.apache.cordova.engine.SystemWebViewClient;
 import org.json.JSONArray;
 import org.json.JSONException;
 
