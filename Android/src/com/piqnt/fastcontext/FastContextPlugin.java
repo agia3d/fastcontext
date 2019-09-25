9ed9801e487428e71e4eb3a0383f8248ffbe8fb2
agia3d <msn@agia3d.fr>
2019-09-26 00:33:50 +0200

    Update FastContextPlugin.java

--- Android/src/com/piqnt/fastcontext/FastContextPlugin.java
+++ Android/src/com/piqnt/fastcontext/FastContextPlugin.java
@@ -14,6 +14,7 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 
 public class FastContextPlugin extends CordovaPlugin {
